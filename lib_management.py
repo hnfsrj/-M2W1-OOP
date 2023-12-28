e673c40bd9aae0425b965df655faabac70f4e905
Hanif <hnfsrj@gmail.com>
2023-12-28 20:21:30 +0300

    Fix bugs and add features

--- lib_management.py
+++ lib_management.py
@@ -1,3 +1,5 @@
+logged = False
+
 class Book:
     def __init__(self, title, author, isbn, number):
         self.title = title
@@ -33,8 +35,9 @@ class Book:
 
 
 class User:
-    def __init__(self, name, id):
+    def __init__(self, name,password, id):
         self.name = name
+        self.password = password
         self.id = id
         self.added = 0
         self.borrowed = 0
@@ -85,8 +88,8 @@ class Library:
         self.numBooks = 0
         self.userid = 0
 
-    def new_user(self,name):
-        self.users.append(User(name,self.userid))
+    def new_user(self,name,password):
+        self.users.append(User(name,password,self.userid))
         self.userid += 1
 
         self.numUsers += 1
@@ -105,10 +108,8 @@ class Library:
 
         self.numBooks += 1
 
-        print(user.name)
 
         return user.add(number)
-        # return user.details()
        
 
 
@@ -124,21 +125,43 @@ class Transaction:
 
 
     def add_book(self, title, author, isbn, number=1):
-        self.total_transactions += 1
-        return zlib.new_book(title, author, isbn, number, self.user)
+        if number>=1:
+            self.total_transactions += 1
+            return zlib.new_book(title, author, isbn, number, self.user)
 
+        return "Invalid number of books"
 
     def borrow_book(self, title):
-        self.total_transactions += 1
-        self.total_borrowed += 1
-        return self.user.borrow(list(filter(lambda x:x.title == title,zlib.books))[0])
+        ztitle = list(filter(lambda x:x.title == title,zlib.books))
+        
+        if ztitle != []:
+            borrowing = self.user.borrow(ztitle[0])
+            
+            if borrowing == "Done!":
+                self.total_transactions += 1
+                self.total_borrowed += 1
+
+            return borrowing
+
+        return "Unfortunately we dont have that book!"
+
+
 
 
     def return_book(self, title):
-        self.total_transactions += 1
-        self.total_returned += 1
-        return self.user.return_books(list(filter(lambda x:x.title == title,zlib.books))[0])
+        ztitle = list(filter(lambda x:x.title == title,zlib.books))
 
+        if ztitle != []:
+            returning = self.user.return_books(ztitle[0])
+            
+            if returning == "Done!":
+                self.total_transactions += 1
+                self.total_returned += 1
+
+            return returning
+
+        return "Unfortunately we dont recognize that book!"
+        
 
     def report(self):
         return f"""
@@ -149,79 +172,163 @@ class Transaction:
             Total books: {zlib.numBooks}
         """
 
+def printer(txt):
+    print('\n',txt,'\n')
 
 def initialize():
-    global transact, zlib
+    global transact, zlib, logged
 
-    print("Welcome to Leb Leb Library")
+    printer("Welcome to Leb Leb Library")
 
     while True:
-        print("1-Create an account","2-Login to an account","3-Add a book","4-Borrow a book","5-Return a book","6-View book details","7-Get transactions report","other-Quit",end='\n')
-        choice1 = int(input(":"))
+        [print(i,end='\n') for i in ["\n1-Create an account","2-Login/change to an account","3-Add a book","4-Borrow a book","5-Return a book","6-View book details","7-Data about user","8-List of available books","9-Get transactions report","10-Logout","other-Quit"]]
+        
+        try:
+            choice1 = int(input(":"))
+        except:
+            break
 
         if choice1 == 1:
             name = input("Enter your name: ")
-            print(zlib.new_user(name))
+            password = input("Create a password: ")
+
+            printer(zlib.new_user(name, password))
 
 
         elif choice1 == 2:
-            id = int(input("Enter your Id to login: "))
+            id = int(input("Enter your Id: "))
+            password = input("Enter your password: ")
 
-            if id<zlib.numUsers:
-                transact = Transaction(list(filter(lambda x: x.id == id,zlib.users))[0])
-                print("Logged in successfully!")
-                print(transact.user)
+            if id<zlib.numUsers and id>=0:
+                theuser = list(filter(lambda x: x.id == id,zlib.users))[0]
+
+                if theuser.password == password:
+                    transact = Transaction(theuser)
+                    logged = True
+                    printer("Logged in successfully!")
+                else:
+                    printer("Wrong Id or password")
             else:
-                print("Invalid Id!")
+                printer("Invalid Id!")
 
         elif choice1 == 3:
-            if transact:
+            if logged:
                 title = input("Enter the title of the book: ")
                 author = input("Enter the author of the book: ")
                 isbn = input("Enter the isbn of the book: ")
                 number = int(input("How many of this book do you want to add: "))
                 
-                print(transact.add_book(title,author,isbn,number))
+                printer(transact.add_book(title,author,isbn,number))
 
             else:
-                print("Please login to an account first!")
+                printer("Please login to an account first!")
 
         elif choice1 == 4:
-            if transact:
+            if logged:
                 title = input("Enter the title of the book: ")
                 
-                print(transact.borrow_book(title))
+                printer(transact.borrow_book(title))
 
             else:
-                print("Please login to an account first!")
+                printer("Please login to an account first!")
 
         elif choice1 == 5:
-            if transact:
+            if logged:
                 title = input("Enter the title of the book: ")
                 
-                print(transact.return_book(title))
+                printer(transact.return_book(title))
 
             else:
-                print("Please login to an account first!")
+                printer("Please login to an account first!")
 
         elif choice1 == 6:
-            if transact:
+            if logged:
                 title = input("Enter the title of the book: ")
-                
-                print(list(filter(lambda x:x.title == title,zlib.books))[0].details())
+
+                ztitle = list(filter(lambda x:x.title == title,zlib.books))
+
+                if ztitle != []:
+                    printer(ztitle[0].details())
+                else:
+                    printer("Unfortunately we dont recognize that book!")
+
 
             else:
-                print("Please login to an account first!")
+                printer("Please login to an account first!")
 
         elif choice1 == 7:
-            if transact:
-                print(transact.report())
+            if logged:
+                printer(transact.user.details())
+
+
             else:
-                print("Please login to an account first!")
+                printer("Please login to an account first!")
+
+        elif choice1 == 8:
+                if zlib.books == []:
+                    printer("There are no books in the library!")
+                else:
+                    
+                    max = zlib.numBooks-1
+                    min = 0
+                    page = 0
+                    zend = False
+
+
+                    if max>=1:
+                        printer("n-Next  |  p-Previous  |  other-Exit")
+                        go = True
+                    else:
+                        go = False
+
+                    printer(zlib.books[page].details())
+                    
+                    while go:
+
+                        choice2 = input(f"Page {page+1} of {max+1} (n/p): ")
+
+                        if choice2 == "n":
+                            if page < max:
+                                if not zend:
+                                    page += 1
+
+                                zend=False
+
+                                printer(zlib.books[page].details())
+                            else:
+                                printer("You've already reached the end!")
+                                zend = True
+                        elif choice2 == "p":
+                            if page > min:
+                                if not zend:
+                                    page -= 1
+
+                                zend=False
+                                
+                                printer(zlib.books[page].details())
+                            else:
+                                printer("You've already reached the end!")
+                                zend = True
+                        else:
+                            break
+
+
+        elif choice1 == 9:
+            if logged:
+                printer(transact.report())
+            else:
+                printer("Please login to an account first!")
+        elif choice1 == 10:
+            if logged:
+                transact = ''
+                logged = False
+                printer("Logged out of account successfully!")
+            else:
+                printer("Please login to an account first!")
         else:
             break
 
 
 
 initialize()
-print('Thank you for using Leb Leb')
+printer('Thank you for using Leb Leb')
